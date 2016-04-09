db13a01604b1d469364b283e445e40811bdcc415
JosephGreaney <josephgreaney@gmail.com>
2016-04-09 17:55:03 +0100

    Added Seeker
    Added the seeker enemy type to the game. When the player picks up the
    object at the end of a level the seeker will begin to chase the player
    through the level from right to left.

--- Assets/ProjectJumpAssets/Scripts/PickUpScript.cs
+++ Assets/ProjectJumpAssets/Scripts/PickUpScript.cs
@@ -3,14 +3,18 @@ using System.Collections;
 
 public class PickUpScript : MonoBehaviour {
 
-	// Use this for initialization
-	void Start ()
-    {
-	}
-	
 	// Update is called once per frame
 	void Update ()
     {
         transform.Rotate(new Vector3(0, 0, 30) * Time.deltaTime);
 	}
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+        {
+            GameObject seeker = GameObject.FindGameObjectWithTag("Seeker");
+            seeker.SendMessage("Move");
+        }
+    }
 }
---
+++ Assets/ProjectJumpAssets/Scripts/Seeker.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Seeker : MonoBehaviour {
+
+    public float speed = 0.2f;
+    private bool move = false;
+	// Update is called once per frame
+	void FixedUpdate ()
+    {
+        if (move)
+        {
+            //Keep moving left
+            transform.position = new Vector3(transform.position.x - (speed), transform.position.y, transform.position.z);
+        }
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.tag == "Player")
+            GameManager.PlayerDeath();
+    }
+
+    void Move()
+    {
+        move = true;
+    }
+}
