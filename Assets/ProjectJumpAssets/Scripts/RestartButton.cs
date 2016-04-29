bab04b1c962559c3428ec5ffe786b3a64c4b270f
jamsiedaly <14152789@studentmail.ul.ie>
2016-04-29 01:56:06 +0100

    Main Menu

--- Assets/ProjectJumpAssets/Scripts/RestartButton.cs
+++ Assets/ProjectJumpAssets/Scripts/RestartButton.cs
@@ -24,6 +24,6 @@ public class RestartButton : MonoBehaviour, IPointerDownHandler
 
     public void restartScene()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene(1);
     }
 }
