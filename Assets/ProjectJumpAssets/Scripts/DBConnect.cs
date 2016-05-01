f0c2ddfc93ea1f34d9d7d3dbe2b6ea6f259f0ba4
JosephGreaney <josephgreaney@gmail.com>
2016-05-01 13:06:55 +0100

    Fixed SQLite Script closing

--- Assets/ProjectJumpAssets/Scripts/DBConnect.cs
+++ Assets/ProjectJumpAssets/Scripts/DBConnect.cs
@@ -11,39 +11,35 @@ public class DBConnect : MonoBehaviour{
     {
         Debug.Log("hs");
         List<DateTime> times = new List<DateTime>();
-        string path = Application.dataPath + "/Database.s3db"; //Path to database.
+        string path = Application.persistentDataPath + "/Database.s3db"; //Path to database.
         Debug.Log(path + "                ");
         if (File.Exists(path))
         {
+            string sqlQuery;
             Debug.Log("Exists");
             string conn = "URI=file:" + path;
-            IDbConnection dbconn;
-            dbconn = (IDbConnection)new SqliteConnection(conn);//creating connection to sqllite database
-            dbconn.Open(); //Open connection to the database.
-            string sqlQuery;
-            IDbCommand dbcmd = dbconn.CreateCommand();
-            sqlQuery = "CREATE TABLE IF NOT EXISTS HighScores(RunTime DATETIME)";
-            dbcmd.CommandText = sqlQuery; //puts query we want to execute into dbcmd
-            dbcmd.ExecuteNonQuery();
-            sqlQuery = "SELECT * " + "FROM HighScores";
-            dbcmd.CommandText = sqlQuery; //puts query we want to execute in
-            IDataReader reader = dbcmd.ExecuteReader(); //executes query
-           /* sqlQuery = "SELECT * " + "FROM HighScores";
-            dbcmd.CommandText = sqlQuery; //puts query we want to execute into dbcmd
-            reader = dbcmd.ExecuteReader(); //executes query*/
-
-            DateTime value;
-            while (reader.Read())
+            using (SqliteConnection dbconn = new SqliteConnection(conn))
             {
-                value = reader.GetDateTime(0); //passing 0 as we are reading from first column in 
-                times.Add(value);
+                dbconn.Open();
+                sqlQuery = "CREATE TABLE IF NOT EXISTS HighScores(RunTime DATETIME)";
+                using (IDbCommand cmd = (IDbCommand)new SqliteCommand(sqlQuery, dbconn))
+                {
+                    cmd.ExecuteNonQuery();
+                }
+                sqlQuery = "SELECT * " + "FROM HighScores";
+                using (IDbCommand cmd = (IDbCommand)new SqliteCommand(sqlQuery, dbconn))
+                {
+                    using (IDataReader reader = cmd.ExecuteReader())
+                    {
+                        DateTime value;
+                        while (reader.Read())
+                        {
+                            value = reader.GetDateTime(0); //passing 0 as we are reading from first column in 
+                            times.Add(value);
+                        }
+                    }
+                }
             }
-            reader.Close();
-            reader = null;
-            dbcmd.Dispose();
-            dbcmd = null;
-            dbconn.Close();
-            dbconn = null;
             times.Sort();
         }
         return times;
@@ -52,70 +48,86 @@ public class DBConnect : MonoBehaviour{
     public static void write(String toBeAdded) //used when there are less than 5 highscores in the database, so score is added without one being removed
     {
         Debug.Log("Write");
-        string path = Application.dataPath + "/Database.s3db";//Path to database.
-         Debug.Log(path);
-        IDbConnection dbconn;
+        string path = Application.persistentDataPath + "/Database.s3db";//Path to database.
+        Debug.Log(path);
+        //IDbConnection dbconn;
         string conn = "URI=file:" + path;
-        dbconn = (IDbConnection)new SqliteConnection(conn);
-        dbconn.Open(); //Open connection to the database.
         string sqlQuery;
-        IDbCommand createcmd = dbconn.CreateCommand();
-        sqlQuery = "CREATE TABLE IF NOT EXISTS HighScores (RunTime DATETIME)";
-        createcmd.CommandText = sqlQuery;
-        IDbCommand dbcmd = dbconn.CreateCommand();
-        sqlQuery = "INSERT INTO HighScores " + "VALUES('" + toBeAdded + "')"; //query broken up for readibility
-        dbcmd.CommandText = sqlQuery;
-        dbcmd.ExecuteNonQuery(); //ExecuteNonQuery excutes an SQL command that does not return a result set
-        dbcmd.Dispose();
-        dbcmd = null;
-        dbconn.Close();
-        dbconn = null;
+        using (SqliteConnection dbconn = new SqliteConnection(conn))
+        {
+            dbconn.Open();
+            sqlQuery = "CREATE TABLE IF NOT EXISTS HighScores(RunTime DATETIME)";
+            using (IDbCommand cmd = (IDbCommand)new SqliteCommand(sqlQuery, dbconn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+            sqlQuery = "INSERT INTO HighScores " + "VALUES('" + toBeAdded + "')"; //query broken up for readibility
+            using (IDbCommand cmd = (IDbCommand)new SqliteCommand(sqlQuery, dbconn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
     }
 
     public static void write(String toBeRemoved, String toBeAdded)
-    {//if we get to here, DB must already exist
+    {
         Debug.Log("write&&&");
-        string conn = "URI=file:" + Application.dataPath + "/Database.s3db"; //Path to database.
-        IDbConnection dbconn;
-        dbconn = (IDbConnection)new SqliteConnection(conn);
-        dbconn.Open(); //Open connection to the database.
-        IDbCommand dbcmd = dbconn.CreateCommand();
-        string sqlQuery = "INSERT INTO HighScores " + "VALUES('" + toBeAdded + "')";
-        dbcmd.CommandText = sqlQuery;
-        dbcmd.ExecuteNonQuery();
-        sqlQuery = "DELETE FROM HighScores " + "WHERE RunTime = '" + toBeRemoved + "'";
-        dbcmd.CommandText = sqlQuery;
-        dbcmd.ExecuteNonQuery();
-        dbcmd.Dispose();
-        dbcmd = null;
-        dbconn.Close();
-        dbconn = null;
+
+        string path = Application.persistentDataPath + "/Database.s3db";//Path to database.
+        Debug.Log(path);
+        //IDbConnection dbconn;
+        string conn = "URI=file:" + path;
+        string sqlQuery;
+        using (SqliteConnection dbconn = new SqliteConnection(conn))
+        {
+            dbconn.Open();
+            sqlQuery = "CREATE TABLE IF NOT EXISTS HighScores(RunTime DATETIME)";
+            using (IDbCommand cmd = (IDbCommand)new SqliteCommand(sqlQuery, dbconn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+            sqlQuery = "INSERT INTO HighScores " + "VALUES('" + toBeAdded + "')"; //query broken up for readibility
+            using (IDbCommand cmd = (IDbCommand)new SqliteCommand(sqlQuery, dbconn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+            sqlQuery = "DELETE FROM HighScores " + "WHERE RunTime = '" + toBeRemoved + "'";
+            using (IDbCommand cmd = (IDbCommand)new SqliteCommand(sqlQuery, dbconn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+        }
     }
 
     public static String getBestTime()
     {
-        string path = Application.dataPath + "/Database.s3db"; //Path to database.
-        string conn = "URI=file:" + path;
+        string path = Application.persistentDataPath + "/Database.s3db"; //Path to database.
+        string sqlQuery;
         DateTime min = new DateTime(2000, 01, 01, 00, 00, 00);
         if (!File.Exists(path))
             return TimeScript.getTimeOnly(min);
-        IDbConnection dbconn;
-        dbconn = (IDbConnection)new SqliteConnection(conn);//creating connection to database
-        dbconn.Open(); //Open connection to the database.
-        IDbCommand dbcmd = dbconn.CreateCommand();
-        string sqlQuery = "SELECT MIN(RunTime) " + "FROM HighScores";
-        dbcmd.CommandText = sqlQuery;
-        IDataReader reader = dbcmd.ExecuteReader();
-        while (reader.Read())
+        string conn = "URI=file:" + path;
+        using (SqliteConnection dbconn = new SqliteConnection(conn))
         {
-            min = reader.GetDateTime(0);
+            dbconn.Open();
+            sqlQuery = "CREATE TABLE IF NOT EXISTS HighScores(RunTime DATETIME)";
+            using (IDbCommand cmd = (IDbCommand)new SqliteCommand(sqlQuery, dbconn))
+            {
+                cmd.ExecuteNonQuery();
+            }
+            sqlQuery = "SELECT MIN(RunTime) " + "FROM HighScores";
+            using (IDbCommand cmd = (IDbCommand)new SqliteCommand(sqlQuery, dbconn))
+            {
+                using (IDataReader reader = cmd.ExecuteReader())
+                {
+                    DateTime value;
+                    while (reader.Read())
+                    {
+                        min = reader.GetDateTime(0);
+                    }
+                }
+            }
         }
-        reader.Close();
-        reader = null;
-        dbcmd.Dispose();
-        dbcmd = null;
-        dbconn.Close();
-        dbconn = null;
         return TimeScript.getTimeOnly(min); //parses timedate as time
     }
 }
